972897208e8fe225cd23959d91492dc7f300670f
mvanger <mvanger@nyu.edu>
2013-07-15 10:30:09 -0400

    created migration file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twittr::Application.config.secret_token = '6d8c22c6faa940f4ad088b76a3a38b58be7b4c1b774c5c15c6ed30e8574b95f7e8f0f47b5cbb2273e79347beabbf01f6844bc6e8323d6c14df7669d50f105ed1'
---
+++ db/migrate/20130715142439_create_user.rb
@@ -0,0 +1,13 @@
+class CreateUser < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :screenname
+      t.string :description
+      t.string :email
+      t.string :location
+      t.string :password
+      t.timestamps
+    end
+  end
+end
