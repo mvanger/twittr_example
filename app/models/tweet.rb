6957d1b042dc74eeb5ddaef6097eab99d0fbc02f
mvanger <mvanger@nyu.edu>
2013-07-15 11:54:07 -0400

    added validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,7 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
   has_many :favorites
   attr_accessible :text
+
+  validates :text, presence: true
+  validates :text, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   has_many :followings
   has_many :friends, through: :following, source: 'followed'
   attr_accessible :name, :screenname, :description, :email, :location, :password
+
+  validates :screenname, presence: true
+  validates :screenname, uniqueness: true
+  validates :password, length: { minimum: 8 }
+  validates :screenname, :password, :name, :description, :location, :email, length: { maximum: 100 }
+  # validates :screenname, :password, :name, :description, :location, :email,
 end
